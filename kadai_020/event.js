c1cf238bf17afd068c817eb57754ff4fa86554f2
renyui-programming <163693699+renyui-programming@users.noreply.github.com>
2024-09-05 14:50:12 +0900

    課題020をアップロード

---
+++ kadai_020/event.js
@@ -0,0 +1,7 @@
+const clickBtn = document.getElementById('btn');
+const textBtn = document.getElementById('text');
+
+clickBtn.addEventListener('click', () =>{
+  
+  textBtn.textContent = 'ボタンをクリックしました';
+});
